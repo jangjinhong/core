cb5875bfa49a70d6f2356e9bcccdd00ac5c941df
jangjinhong <honge1122@naver.com>
2024-01-08 14:58:24 +0900

    섹션 3. 컨테이너로 전환

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,19 +1,33 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
-    public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
-    } // 원래는 이걸 서비스에 직접 써줌 A a = new a-1(); 의 형태로
-    // 즉, new로 가져올 리포지토리를 서비스가 직접 정함(배우가 캐스팅;;한꼴) 이걸 여기다가 해주겠다
 
+    @Bean
+    public MemberService memberService() {
+        return new MemberServiceImpl(memberRepository());
+    }
+    @Bean
+    public MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+    @Bean
     public OrderService orderService() { //얘는 매개변수가 2개가 필요함 정책과 저장소
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+    @Bean
+    public DiscountPolicy discountPolicy() {
+        return new RateDiscountPolicy();
     }
 }
