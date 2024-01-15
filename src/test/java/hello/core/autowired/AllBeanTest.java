f0b081c563f6a246815a869e6c06913125e488f5
jangjinhong <honge1122@naver.com>
2024-01-15 15:24:14 +0900

    섹션 8. 빈 생명주기 콜백

--- src/test/java/hello/core/autowired/AllBeanTest.java
+++ src/test/java/hello/core/autowired/AllBeanTest.java
@@ -4,8 +4,6 @@ import hello.core.AutoAppConfig;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Grade;
 import hello.core.member.Member;
-import hello.core.order.OrderService;
-import hello.core.order.OrderServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
